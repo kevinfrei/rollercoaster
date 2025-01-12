5da87b517bbedb649aef64b23effa547874fde38
Kevin Frei <kevinfrei@hotmail.com>
2025-01-12 08:10:27 -0800

    Okay, ready to move to main portion

--- coaster-ts-jotai/src/App.tsx
+++ coaster-ts-jotai/src/App.tsx
@@ -1,34 +1,32 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import {
+  FluentProvider,
+  webLightTheme,
+  webDarkTheme,
+  Button,
+} from '@fluentui/react-components';
+import { MathJaxContext } from 'better-react-mathjax';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [light, setLight] = useState(false);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <FluentProvider theme={light ? webLightTheme : webDarkTheme}>
+      <MathJaxContext>
+        <h1>Getting started</h1>
+        <div className="card">
+          <Button appearance='primary' onClick={() => setLight((l) => !l)}>
+            Theme is {light ? 'light' : 'dark'}
+          </Button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </MathJaxContext>
+    </FluentProvider>
   );
 }
 
